e36b1e6e0f851122ae318cd42f0318b4d354b603
runebaas <runebaas@gmail.com>
2019-09-28 16:33:16 +0200

    Beautify html output

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import * as path from 'path';
 import * as parse5 from 'parse5';
 import { domManager } from './lib/vdom/domHandler';
 import { TreeElement } from './models/treeElementModel';
+import { html_beautify } from 'js-beautify';
 
 (async () => {
   const root = await parseComponentInfo(path.resolve('./example'));
@@ -12,6 +13,12 @@ import { TreeElement } from './models/treeElementModel';
     useCssModules: false,
     variables: root.propData
   });
-  const final = parse5.serialize(res);
+  const html = parse5.serialize(res);
+  const final = html_beautify(html, {
+    end_with_newline: true,
+    indent_body_inner_html: true,
+    indent_size: 2,
+    preserve_newlines: false
+  });
   console.log(final);
 })().catch(console.error);
