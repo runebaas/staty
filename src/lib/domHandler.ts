dd94f924bc2ad201b85cd9d920d39e75ab0fb89c
runebaas <runebaas@gmail.com>
2019-09-29 23:06:07 +0200

    Add attribute substitution

--- src/lib/domHandler.ts
+++ src/lib/domHandler.ts
@@ -1,32 +1,53 @@
 import { TreeElement } from '../models/treeElementModel';
 import { Scope } from '../models/scopeModel';
 import { nodeHandlerFunctions } from './nodeHandlers/handlers';
+import { Attribute } from 'parse5';
 
 export async function domManager(doc: TreeElement, scope: Scope): Promise<TreeElement> {
-  const parseFunction = nodeHandlerFunctions[doc.nodeName];
+  const newDoc = doc;
+
+  newDoc.attrs = substituteAttributes(newDoc.attrs, scope);
+
+  const parseFunction = nodeHandlerFunctions[newDoc.nodeName];
   if (parseFunction !== undefined) {
-    return parseFunction(doc, scope);
+    return parseFunction(newDoc, scope);
   }
 
-  if (doc.childNodes === undefined) {
-    return doc;
+  if (newDoc.childNodes === undefined) {
+    return newDoc;
   }
 
-  const results = (await Promise.all(doc.childNodes.map(node => domManager(node, {
-    ...scope
-  })))).filter(d => d !== null);
+  const results = (await Promise.all(newDoc.childNodes.map(node => domManager(node, scope)))).filter(d => d !== null);
 
   if (results.length === 0) {
-    return doc;
+    return newDoc;
   }
 
-  const nodes = results
+  newDoc.childNodes = results
     .map(r => [r])
     .map(r => r[0].nodeName === 'rplc' ? r[0].childNodes : r)
     .flat();
 
-  const newDoc = doc;
-  newDoc.childNodes = nodes;
   return newDoc;
 }
 
+function substituteAttributes(attributes: (Attribute[]|undefined), scope: Scope): Attribute[] {
+  if (attributes === undefined || attributes.length === 0 || attributes.find(a => !a.name.startsWith(':'))) { return attributes || []; }
+
+  const variables = {
+    ...scope.globalVariables,
+    ...scope.variables
+  };
+
+  return attributes.map(attr => {
+    if (attr.name.startsWith(':')) {
+      attr.name = attr.name.slice(1);
+      if (variables[attr.value]) {
+        attr.value = variables[attr.value];
+      } else {
+        console.info(scope.path, 'Unable to replace prop', attr);
+      }
+    }
+    return attr;
+  });
+}
--- src/lib/nodeHandlers/textHandler.ts
+++ src/lib/nodeHandlers/textHandler.ts
@@ -4,20 +4,19 @@ import { GenerateErrorNode } from '../errorGenerators';
 
 export function HandleText(doc: TreeElement, scope: Scope): TreeElement {
   const textNode = doc;
-  const variables = {
-    ...scope.globalVariables,
-    ...scope.variables
-  };
   try {
     const matches = textNode.value.match(/{{(.+)}}/);
     if (matches !== null) {
+      const variables = {
+        ...scope.globalVariables,
+        ...scope.variables
+      };
       const key = matches[1].trim();
       if (Object.keys(variables).includes(key)) {
         textNode.value = variables[key];
       }
     }
   } catch (e) {
-    console.error('text substitution failed', doc.value, variables, e.message);
     GenerateErrorNode('text substitution failed', scope.path, e);
   }
 
