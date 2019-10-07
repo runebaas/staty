c464cff67988232f9e89fc0ab01901a57442e89a
runebaas <runebaas@gmail.com>
2019-10-08 00:40:31 +0200

    Expose the plugin api and slight main.ts tweaks

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,26 @@
 import { Compiler, } from './compiler';
-import { PluginManager, } from './core/modules/pluginManager';
 import { CompilerOptions, } from './compilerOptions';
+import { PluginManager, } from './core/modules/pluginManager';
 import { ModuleManager, } from './core/modules/moduleManager';
 import { MessageManager, } from './core/modules/messageManager';
 
 export class Staty {
   private readonly options: StatyOptions;
-  private moduleManager: ModuleManager;
+  private readonly moduleManager: ModuleManager;
+  private readonly pluginManager: PluginManager;
+  public readonly messages: MessageManager;
 
-  constructor(options: StatyOptions) {
+  constructor(options: StatyOptions = {}) {
     this.options = options;
-    this.initModules();
-  }
 
-  private initModules(): void {
-    const pluginManager = new PluginManager();
-    pluginManager.addDefaultPlugins();
+    this.pluginManager = new PluginManager();
+    this.pluginManager.addDefaultPlugins();
 
-    const messageManager = new MessageManager();
+    this.messages = new MessageManager();
 
     this.moduleManager = new ModuleManager({
-      pluginManager,
-      messageManager,
+      pluginManager: this.pluginManager,
+      messageManager: this.messages,
     });
   }
 
@@ -29,6 +28,10 @@ export class Staty {
     const compiler = new Compiler(this.options.compiler, this.moduleManager);
 
     return compiler.compile(filePath);
+  }
+
+  public addPlugin(pluginFunc: (pluginManager: PluginManager) => void): void {
+    pluginFunc(this.pluginManager);
   }
 }
 
--- tests/fullTest/full.test.ts
+++ tests/fullTest/full.test.ts
@@ -5,7 +5,7 @@ import * as path from 'path';
 describe('Full Test', () => {
   test('Renders the example correctly', async () => {
     const expected = await readFile(path.resolve(path.dirname(__filename), './expectedResult.html'));
-    const staty = new Staty({});
+    const staty = new Staty();
     const result = await staty.compile('./example/index.staty');
     expect(result).toBe(expected.toString());
   });
